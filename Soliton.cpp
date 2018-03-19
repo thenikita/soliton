e989fb63f965f830fc95f52d6d44a636ea58b997
nick theone <patokpatuwka@outlook.com>
2018-03-19 23:15:21 +0500

    Initial commit.

---
+++ Soliton.cpp
@@ -0,0 +1,5 @@
+//
+// Created by User on 3/19/2018.
+//
+
+#include "Soliton.h"
---
+++ Soliton.h
@@ -0,0 +1,14 @@
+//
+// Created by User on 3/19/2018.
+//
+
+#ifndef SOLITON_SOLITON_H
+#define SOLITON_SOLITON_H
+
+
+class Soliton {
+
+};
+
+
+#endif //SOLITON_SOLITON_H
